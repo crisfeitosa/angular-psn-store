d57d55aeb1c25687127ba8372a691bfd2b77d4a5
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-03-15 13:13:46 -0300

    feat: update menu bar with logo and styling, add asset files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MenuBar } from "./components/menu-bar/menu-bar";
 import { Home } from "./pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuBar, Home],
+  imports: [MenuBar, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
